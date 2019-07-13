d0fe2773619cdd8bdd7337b017997db913fc15f9
huofangLi <2895844821@qq.com>
2019-07-13 09:48:33 +0800

    项目创建

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'userInfo', component: UserInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
